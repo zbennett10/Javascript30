dcd516dd3c58fe5b3377fe22304cfd09d6f6b87c
Zachary Bennett <zbennett10@Hotmail.com>
2016-12-16 10:53:29 -0500

    Create DaySeven-ArrayCardioTwo.js

---
+++ DaySeven-ArrayCardioTwo.js
@@ -0,0 +1,37 @@
+<script>
+    // ## Array Cardio Day 2
+
+    const people = [
+      { name: 'Wes', year: 1988 },
+      { name: 'Kait', year: 1986 },
+      { name: 'Irv', year: 1970 },
+      { name: 'Lux', year: 2015 }
+    ];
+
+    const comments = [
+      { text: 'Love this!', id: 523423 },
+      { text: 'Super good', id: 823423 },
+      { text: 'You are the best', id: 2039842 },
+      { text: 'Ramen in my fav food ever', id: 123523 },
+      { text: 'Nice Nice Nice!', id: 542328 }
+    ];
+
+    // Some and Every Checks
+    // Array.prototype.some() // is at least one person 19?
+    var under19 = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+  
+    // Array.prototype.every() // is everyone 19?
+    var everyOneOver19 = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+    // Array.prototype.find()
+    // Find is like filter, but instead returns just the one you are looking for
+    // find the comment with the ID of 823423
+    var myID = comments.find(comment => comment.id === 823423);
+
+    // Array.prototype.findIndex()
+    // Find the comment with this ID
+    // Delete the comment with the id of 823423
+   comments.splice(comments.findIndex(comment=>comment.id===823423), 1);
+
+  </script>
